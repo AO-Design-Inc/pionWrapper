3c4873b0500f231f448300a8bf7f450fb148c945
Abhishek Cherath <abhi@feather.systems>
2022-03-25 13:05:30 -0400

    add SetRemoteDescription to pionwrapper

--- src/pionWrapper.c
+++ src/pionWrapper.c
@@ -4,6 +4,36 @@
 #include "../go-src/pionHandler.h"
 #define BUFSIZE 10000
 
+napi_value CSetRemoteDescription(napi_env env, napi_callback_info info) {
+	napi_status status;
+	size_t argc = 1;
+	napi_value argv[1];
+	status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
+	if (status != napi_ok) {
+		napi_throw_error(env, NULL, "Failed to parse args");
+	}
+
+	char* remoteSDP = malloc(BUFSIZE); 
+	size_t result;
+
+	status = napi_get_value_string_utf8(env, argv[0], remoteSDP, BUFSIZE, &result);
+	if (status != napi_ok) {
+		napi_throw_error(env, NULL, "Invalid string passed in as arg");
+	}
+
+	GoSlice remoteSDPGo = {remoteSDP, result, result};
+
+	
+	bool remoteSetOk = (bool) SetRemoteDescription(remoteSDPGo);
+	napi_value RemoteDescriptionOk;
+	status = napi_get_boolean(env, remoteSetOk, &RemoteDescriptionOk); 
+	if (status == napi_ok) { 
+		napi_throw_error(env, NULL, "bad remote sdp");
+	}
+	return RemoteDescriptionOk;
+}
+
+
 napi_value StartPionScreenShare(napi_env env, napi_callback_info info) {
 	napi_status status;
 	size_t argc = 1;
@@ -16,7 +46,6 @@ napi_value StartPionScreenShare(napi_env env, napi_callback_info info) {
 
 	char* iceServers = malloc(BUFSIZE); 
 	size_t result;
-	//not sure what this result is for?
 
 	status = napi_get_value_string_utf8(env, argv[0], iceServers,BUFSIZE, &result);
 	if (status != napi_ok) {
@@ -38,7 +67,6 @@ napi_value StartPionScreenShare(napi_env env, napi_callback_info info) {
 }
 
 
-
 napi_value Init(napi_env env, napi_value exports) {
 	napi_status status;
 	napi_value fn;
@@ -50,6 +78,17 @@ napi_value Init(napi_env env, napi_value exports) {
 	status = napi_set_named_property(env, exports, "PionOffer", fn);
 	if (status != napi_ok) {
 		napi_throw_error(env, NULL, "Unable to populate exports");
+	}
+
+	napi_value fn2;
+	status = napi_create_function(env, NULL, 0, CSetRemoteDescription, NULL, &fn2);
+	if (status != napi_ok) {
+		napi_throw_error(env, NULL, "unable to create function SetRemoteDescription");
+	}
+
+	status = napi_set_named_property(env, exports, "SetRemoteDescription", fn2);
+	if (status != napi_ok) {
+		napi_throw_error(env, NULL, "Unable to create SetRemoteDescription");
 	}
 
 	return exports;
